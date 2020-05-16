a85d8c4ff391517f5798a32d3c5632915fc5c527
Singhatul9936 <53752117+Singhatul9936@users.noreply.github.com>
2020-05-16 11:37:08 +0530

    Add files via upload

---
+++ Python Data Structures/Chapter 10/Assignment 10.2.py
@@ -0,0 +1,17 @@
+
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+a = handle.read()
+b = a.split("\n")
+d = []
+for i in b:
+    if i.startswith("From "):
+        c = i.split(":")
+        d.append(c[0][-2:])
+d.sort()
+counts = {}
+for j in d:
+    counts[j] = d.count(j)
+for k, l in counts.items():
+    print(k, l)
---
+++ Python Data Structures/Chapter 6/Assignment 6.5.py
@@ -0,0 +1,6 @@
+
+text = "X-DSPAM-Confidence:    0.8475";
+a = text[-6:]
+b = float(a)
+print(b)
+text.find(":")
